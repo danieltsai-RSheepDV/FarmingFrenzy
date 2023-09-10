9c62f0fafecadbb9404b07ade519ed37c45f95b8
wathmew <77905462+wathmew@users.noreply.github.com>
2023-09-10 01:29:19 -0700

    tilemap + some script sketches

---
+++ Assets/Player/Farming/happy harvest script theft/cropHH.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cropHH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+/*
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.VFX;
+
+namespace HappyHarvest
+{
+    /// <summary>
+    /// Class used to designated a crop on the map. Store all the stage of growth, time to grow etc.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Crop", menuName = "2D Farming/Crop")]
+    public class Crop : ScriptableObject, IDatabaseEntry
+    {
+        public string Key => UniqueID;
+
+        public string UniqueID = "";
+        
+        public TileBase[] GrowthStagesTiles;
+
+        public Product Produce;
+        
+        public float GrowthTime = 1.0f;
+        public int NumberOfHarvest = 1;
+        public int StageAfterHarvest = 1;
+        public int ProductPerHarvest = 1;
+        public float DryDeathTimer = 30.0f;
+        public VisualEffect HarvestEffect;
+
+        public int GetGrowthStage(float growRatio)
+        {
+            return Mathf.FloorToInt(growRatio * (GrowthStagesTiles.Length-1));
+        }
+    }
+}
+
+*/
