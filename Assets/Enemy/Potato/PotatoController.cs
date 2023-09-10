ebdb7bff8484b7df33f43d0eafb3f22313684084
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2023-09-09 22:45:17 -0700

    potato

---
+++ Assets/Enemy/Potato/PotatoController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoController : MonoBehaviour
+{
+    enum States
+    {
+        ROLLING,
+        IDLE
+    }
+
+    private States curState = States.IDLE;
+    
+    const float SMALL_NUMBER = 0.1f;
+
+    [SerializeField] public SpriteRenderer sp;
+    [Space] 
+    [SerializeField] private float damageAmount;
+    [Space]
+    [SerializeField] public float speed;
+    [SerializeField] public float turnSpeed;
+    [SerializeField] private float driftDistance = 3f;
+    [SerializeField] private float pauseTime = 1f;
+    [SerializeField] private float knockbackAmount;
+    [Space]
+    [SerializeField] public float accel;
+    [SerializeField] public float decel;
+    [SerializeField] public float velPower;
+
+    private float timer;
+    
+    private GameObject target;
+    private Vector3 direction;
+    private float usedAngle;
+    private float distanceTravelled;
+    
+    private Rigidbody2D rb;
+    private Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        timer = pauseTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(rb.velocity.x) > SMALL_NUMBER)
+        {
+            sp.flipX = rb.velocity.x > 0;
+            anim.SetBool("Reversed", rb.velocity.x > 0);
+        }
+
+        if (curState == States.ROLLING)
+        {
+            if (target == null)
+            {
+                curState = States.IDLE;
+                return;
+            }
+            
+            if (Mathf.Abs(usedAngle) < 135)
+            {
+                Vector3 targetDir = (target.transform.position - transform.position).normalized;
+                Vector3 newDir = Vector3.RotateTowards(direction, targetDir, turnSpeed * Time.deltaTime, 0f);
+                usedAngle += Vector3.SignedAngle(newDir, direction, Vector3.forward);
+                direction = newDir;
+                
+                SetVelocity(direction * speed);
+            }
+            else
+            {
+                if (distanceTravelled < driftDistance)
+                {
+                    distanceTravelled += rb.velocity.magnitude * Time.fixedDeltaTime;
+                }else{
+                    curState = States.IDLE;
+                    anim.SetBool("Rolling", false);
+                    
+                    timer = pauseTime;
+                    usedAngle = 0f;
+                    distanceTravelled = 0f;
+                    
+                    return;
+                }
+            }
+        }
+        else if (curState == States.IDLE)
+        {
+            SetVelocity(Vector3.zero);
+            
+            if (timer > 0f)
+            {
+                timer -= Time.deltaTime;
+                return;
+            }
+            
+            target = FindObjectOfType<PlayerMovement>().gameObject;
+            if (target)
+            {
+                curState = States.ROLLING;
+                anim.SetBool("Rolling", true);
+                
+                direction = (target.transform.position - transform.position).normalized;
+            }
+        }
+    }
+
+    private void SetVelocity(Vector2 velocity)
+    {
+        Vector2 curVel = rb.velocity;
+        Vector2 force = Vector2.zero;
+    
+        // X
+        float xDif = velocity.x - curVel.x;
+        float xAccel = (Math.Abs(velocity.x) > SMALL_NUMBER) ? accel : decel;
+        force.x = Mathf.Pow(Mathf.Abs(xDif) * xAccel, velPower) * Mathf.Sign(xDif);
+    
+        // Y
+        float yDif = velocity.y - curVel.y;
+        float yAccel = (Math.Abs(velocity.y) > SMALL_NUMBER) ? accel : decel;
+        force.y = Mathf.Pow(Mathf.Abs(yDif) * yAccel, velPower) * Mathf.Sign(yDif);
+    
+        rb.AddForce(force);
+    }
+
+    private Vector3 Perpendicular(Vector3 a, Vector3 b)
+    {
+        float dot = Vector3.Dot(a, b);
+        Vector3 dotVector = dot * b.normalized;
+        return a - dotVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Health h = col.gameObject.GetComponent<Health>();
+        if (h && col.gameObject.CompareTag("Player"))
+        {
+            h.Damage(damageAmount);
+        }
+        
+        rb.velocity = -rb.velocity.normalized * knockbackAmount;
+        
+        curState = States.IDLE;
+        anim.SetBool("Rolling", false);
+                    
+        timer = pauseTime;
+        usedAngle = 0f;
+        distanceTravelled = 0f;
+    }
+}
