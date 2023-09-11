1201c6643c0231290f38608002665589865070b0
wathmew <77905462+wathmew@users.noreply.github.com>
2023-09-10 17:31:00 -0700

    inv system attempts

---
+++ Assets/Player/Farming/InvStuff/Toolbar_UI.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toolbar_UI : MonoBehaviour
+{
+    [SerializeField] private List<placeholder> toolbarSlots = new List<placeholder>();
+
+    private placeholder selectedSlot;
+
+    private void Start(){
+        SelectSlot(0);
+    }
+
+    private void Update(){
+        CheckAlphaNumericKeys();
+    }
+
+    public void SelectSlot(int index){
+        if(toolbarSlots.Count == 6){ // 6 or however many slots
+            selectedSlot = toolbarSlots[index];
+            Debug.Log("Selected Slot: " + selectedSlot.name);
+        }
+    }
+    
+    private void CheckAlphaNumericKeys(){
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SelectSlot(0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SelectSlot(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SelectSlot(2);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SelectSlot(3);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SelectSlot(4);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SelectSlot(5);
+        }
+
+    }
+}
