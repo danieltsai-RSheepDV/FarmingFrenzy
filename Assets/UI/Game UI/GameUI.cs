0b8ddbc7ea6e06666578d7575b0cdfd224ce01a5
Faustine <faustinex.wang@gmail.com>
2023-09-10 17:55:37 -0700

    inventory

--- Assets/UI/Game UI/GameUI.cs
+++ Assets/UI/Game UI/GameUI.cs
@@ -8,8 +8,10 @@ public class GameUI : MonoBehaviour
     [SerializeField] Slider houseHealth;
     [SerializeField] Health house;
 
-    [SerializeField] GameObject[] invItems;
+    [SerializeField] Image[] invItems; // array of background images
     [SerializeField] InventoryManager invManager;
+
+    // 4A3C43
 
     private void Update()
     {
@@ -21,17 +23,52 @@ public class GameUI : MonoBehaviour
         switch (tool)
         {
             case InventoryManager.Item.Rake:
-                
+                invItems[0].color = Color.red;
+                invItems[1].color = Color.white;
+                invItems[2].color = Color.white;
+                invItems[3].color = Color.white;
+                invItems[4].color = Color.white;
+                invItems[5].color = Color.white;
                 break;
             case InventoryManager.Item.Can:
+                invItems[0].color = Color.white;
+                invItems[1].color = Color.red;
+                invItems[2].color = Color.white;
+                invItems[3].color = Color.white;
+                invItems[4].color = Color.white;
+                invItems[5].color = Color.white;
                 break;
             case InventoryManager.Item.Pea:
+                invItems[0].color = Color.white;
+                invItems[1].color = Color.white;
+                invItems[2].color = Color.red;
+                invItems[3].color = Color.white;
+                invItems[4].color = Color.white;
+                invItems[5].color = Color.white;
                 break;
             case InventoryManager.Item.Potato:
+                invItems[0].color = Color.white;
+                invItems[1].color = Color.white;
+                invItems[2].color = Color.white;
+                invItems[3].color = Color.red;
+                invItems[4].color = Color.white;
+                invItems[5].color = Color.white;
                 break;
             case InventoryManager.Item.Fence:
+                invItems[0].color = Color.white;
+                invItems[1].color = Color.white;
+                invItems[2].color = Color.white;
+                invItems[3].color = Color.white;
+                invItems[4].color = Color.red;
+                invItems[5].color = Color.white;
                 break;
             case InventoryManager.Item.Path:
+                invItems[0].color = Color.white;
+                invItems[1].color = Color.white;
+                invItems[2].color = Color.white;
+                invItems[3].color = Color.white;
+                invItems[4].color = Color.white;
+                invItems[5].color = Color.red;
                 break;
             default:
                 break;
