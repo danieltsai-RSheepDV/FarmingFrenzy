96038a64ecade9b82d8cc0d6cdc50fb46f232b99
Faustine <faustinex.wang@gmail.com>
2023-09-14 20:52:40 -0700

    cleaned up shop code

--- Assets/Levels/Farming/Shop/ShopManager.cs
+++ Assets/Levels/Farming/Shop/ShopManager.cs
@@ -12,29 +12,9 @@ public class ShopManager : MonoBehaviour
     InventoryManager invManager;
     ItemDatabase ItemDatabase;
 
-    const int toolsSkipped = 2; // number of tools before consumables
-    const string moneySymb = "$";
     [SerializeField] private string[] shopIds = new string[] { "Item_PeaSeeds", "Item_PotatoSeeds", "Item_Fence", "Item_Path" }; // items that are in the shop
-    
     [SerializeField] GameObject shopGrid; // the shop's grid
     [SerializeField] GameObject shopItem; // shop item prefab
-
-    // shop item game objects
-    public class ShopItemData
-    {
-        public string itemId;
-        public int index;
-        public float price;
-        public Button buyButton;
-
-        public ShopItemData(string s, int i, float p)
-        {
-            itemId = s;
-            index = i;
-            price = p;
-        }
-    }
-    public List<ShopItemData> shopItems = new List<ShopItemData>(); // stores the shop items
 
     void Start()
     {
@@ -46,32 +26,35 @@ public class ShopManager : MonoBehaviour
 
     private void SetUpShop() // show shop items
     {
+        const string moneySymb = "$";
         for (int i = 0; i < shopIds.Length; i++) // add each item into the shop
         {
-            GameObject newShopItem = Instantiate(shopItem); // instantiate shop item
+            // instantiate shop item
+            GameObject newShopItem = Instantiate(shopItem);
 
             // create items
-            string id = shopIds[i]; // item id
+            string id = shopIds[i]; // store item id
             ItemDatabaseAsset.ItemData itemData = ItemDatabase[id]; // get item data
-            newShopItem.transform.GetChild(0).GetComponent<Image>().sprite = itemData.icon; // show shop item sprite
+
+            // show item sprite
+            newShopItem.transform.GetChild(0).GetComponent<Image>().sprite = itemData.icon;
+
+            // show item price
             float price = itemData.price;
-            newShopItem.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = moneySymb + price.ToString(); // show item price
+            newShopItem.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = moneySymb + price.ToString();
 
-            newShopItem.transform.SetParent(shopGrid.transform); // add shop item to grid
+            // hook up button
+            Button buyButton = newShopItem.transform.GetChild(3).GetComponent<Button>(); // store button
+            buyButton.onClick.AddListener(delegate { BuyItem(id, price); }); // trigger buying of item on click
 
-            // add items to list
-            ShopItemData newStoredShopItem = new ShopItemData(id, i, price); // new ShopItem instantiated
-
-            newStoredShopItem.buyButton = newShopItem.transform.GetChild(3).GetComponent<Button>(); // store button
-            newStoredShopItem.buyButton.onClick.AddListener(delegate { BuyItem(newStoredShopItem); }); // trigger buying of item on click
-
-            shopItems.Add(newStoredShopItem); // add item to list
+            // add shop item to grid
+            newShopItem.transform.SetParent(shopGrid.transform);
         }
     }
 
-    private void BuyItem(ShopItemData shopItem)
-    { 
-        invManager.DecMoney(shopItem.price);
-        invManager.items[shopItem.index + toolsSkipped].amount++; // increase amount
+    private void BuyItem(string itemId, float price)
+    {
+        invManager.DecMoney(price); // decrease money
+        invManager.AddItem(itemId); // increase corresponding amount
     }
 }
